52149f28e47dc509dd2274e5c96fc6e4f371cb2a
Jess Telford <hi+github@jes.st>
2023-05-18 11:23:58 +1000

    Don't do anything interactive (TTY) in CI/SPIN/CODESPACES

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ if ! chezmoi="$(command -v chezmoi)"; then
   bin_dir="${HOME}/.local/bin"
   chezmoi="${bin_dir}/chezmoi"
   echo "Installing chezmoi to '${chezmoi}'" >&2
+  
   if command -v curl >/dev/null; then
     chezmoi_install_script="$(curl -fsSL https://chezmoi.io/get)"
   elif command -v wget >/dev/null; then
@@ -16,8 +17,18 @@ if ! chezmoi="$(command -v chezmoi)"; then
     echo "To install chezmoi, you must have curl or wget installed." >&2
     exit 1
   fi
-  sh -c "${chezmoi_install_script}" -- -b "${bin_dir}"
-  unset chezmoi_install_script bin_dir
+  
+  # +x is to detect an empty env var: https://stackoverflow.com/a/13864829
+  if [ -z "${CODESPACES+x}" ] || [ -z "${CI+x}" ] || [ -z "${SPIN+x}" ]; then
+    # Can't do anything interactive in these environments
+    chezmoi_args="--no-tty --force"
+  else
+    chezmoi_args=""
+  fi
+  
+  sh -c "${chezmoi_install_script}" -- -b "${bin_dir}" "${chezmoi_args}"
+  
+  unset chezmoi_install_script bin_dir chezmoi_args
 fi
 
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
