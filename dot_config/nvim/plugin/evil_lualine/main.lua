64dfeec87978cf9337c56303c4d3e93ea96ad93e
Jess Telford <hi+github@jes.st>
2025-04-01 16:55:02 +1100

    Remove usage of deprecated get_active_clients in neovim statusline code

--- dot_config/nvim/plugin/evil_lualine/main.lua
+++ dot_config/nvim/plugin/evil_lualine/main.lua
@@ -213,7 +213,7 @@ _G.eviline = function()
       -- Lsp server name .
       function()
         local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-        local clients = vim.lsp.get_active_clients()
+        local clients = vim.lsp.get_clients()
         if next(clients) == nil then
           return 'No Active Lsp'
         end
